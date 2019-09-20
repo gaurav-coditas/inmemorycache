70829ade799dd84242833e16a3cd009ea38f23e0
Gaurav Pingale <gaurav.pingale@coditas.com>
2019-09-20 13:31:03 +0530

    initialised project

---
+++ src/main/java/com/coditas/inmemorycache/InmemorycacheApplication.java
@@ -0,0 +1,13 @@
+package com.coditas.inmemorycache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InmemorycacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InmemorycacheApplication.class, args);
+    }
+
+}
